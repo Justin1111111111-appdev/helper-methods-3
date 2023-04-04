b84a89e3e181c18b3782e8833fa070b03638cc14
Justin1111111111 <stuwrkr-jguzm21@mx.uillinois.edu>
2023-04-04 17:22:19 +0000

    Almost done with video 3 helper methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def new
-    @movie = Movie.new
+    @new_movie = Movie.new
   end
 
   def index
@@ -17,9 +17,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params.fetch(:id))
   end
 
-  def create
-    movie_params = params.require(:movie).permit(:title, :description)
-    
+  def create 
     @movie = Movie.new(movie_params)
 
     if @movie.valid?
@@ -32,13 +30,11 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
+    @the_movie = Movie.find(params.fetch(:id))
   end
 
   def update
     @movie = Movie.find(params.fetch(:id))
-
-    movie_params = params.require(:movie).permit(:title, :description)
     
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
@@ -54,4 +50,11 @@ class MoviesController < ApplicationController
 
     redirect_to movies_url, notice: "Movie deleted successfully."
   end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :image_url, :director_id, :released_on)    
+
+  end 
 end
